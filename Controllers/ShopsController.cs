b2b4a107e915a92a5bfe7436c5334b9fc0cee3bd
Микола Матіос <mikolka09@gmail.com>
2021-08-17 21:59:04 +0300

    17.08.2021

---
+++ Controllers/ShopsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HomeWork_Base.Data;
+using HomeWork_Base.Models;
+
+namespace HomeWork_Base.Controllers
+{
+    public class ShopsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shops
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Shops.Include(s => s.Country);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Shops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shops
+                .Include(s => s.Country)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // GET: Shops/Create
+        public IActionResult Create()
+        {
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: Shops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CountryId")] Shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", shop.CountryId);
+            return View(shop);
+        }
+
+        // GET: Shops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shops.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", shop.CountryId);
+            return View(shop);
+        }
+
+        // POST: Shops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CountryId")] Shop shop)
+        {
+            if (id != shop.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShopExists(shop.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", shop.CountryId);
+            return View(shop);
+        }
+
+        // GET: Shops/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shops
+                .Include(s => s.Country)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // POST: Shops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shop = await _context.Shops.FindAsync(id);
+            _context.Shops.Remove(shop);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShopExists(int id)
+        {
+            return _context.Shops.Any(e => e.Id == id);
+        }
+    }
+}
