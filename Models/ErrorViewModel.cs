0eb30e25f3af7a4f42dcf0d609d2823909ff96e5
Микола Матіос <mikolka09@gmail.com>
2021-08-17 21:36:31 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWork_Base.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
